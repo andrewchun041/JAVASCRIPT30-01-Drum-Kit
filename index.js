3f62ee11358162fc9b8b362dffb48553b92630f2
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-03 22:18:22 -0700

    remove index-FINISHED.html from gitignore for similar solution

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop the function from running if no element with data-key associated with the keyCode key pressed is found
+    
     audio.currentTime = 0; // rewind audio to start
     audio.play();
     key.classList.add('playing');
