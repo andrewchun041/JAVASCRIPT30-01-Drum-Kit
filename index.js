b4b94c6529840bfcd3831eac24ee2346412f99da
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-03 22:13:13 -0700

    add event listener for when transition ends in each .key element and call removeTransition function; add removeTransition function that ignores all transition except transform and remove the class 'playing' for the selected element to remove playing styling

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-window.addEventListener('keydown', function(e){
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return; // stop the function from running if no element with data-key associated with the keyCode key pressed is found
     audio.currentTime = 0; // rewind audio to start
     audio.play();
     key.classList.add('playing');
-});
+}
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; // skip if not a transform
+    this.classList.remove('playing');
+}
+
+// play sound when keys are pressed
+window.addEventListener('keydown', playSound); // call playSound function in the event that a key is pressed
+
+// remove playing styling after transition ends
+const keys = document.querySelectorAll('.key'); // store all elements with class .key as an array in the variable keys
+keys.forEach(key => key.addEventListener('transitionend',removeTransition)); // call remove Transition function when transition ends for each element in keys variable
